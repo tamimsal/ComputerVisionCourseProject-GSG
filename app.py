86f15630f37104abf83563cf78b33b994c68365a
Tamim Salhab <tamem222333@gmail.com>
2024-03-21 23:22:05 +0200

    editing strich

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import os
-
+import torch
+from pathlib import Path
+from PIL import Image
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'static'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 if not os.path.exists(UPLOAD_FOLDER):
@@ -21,19 +23,23 @@ def sorted_directory_listing_with_os_listdir(directory):
     sorted_items = sorted(items)
     return sorted_items
 
+
+    
+
+
 def stritchImages():
     stitcher = cv2.Stitcher_create()
-    files = sorted_directory_listing_with_os_listdir('uploads')
-    leftImage = cv2.imread('uploads/' + files[0])
+    files = sorted_directory_listing_with_os_listdir('static')
+    leftImage = cv2.imread('static/' + files[0])
     count = 0
     for file in files[1:]:
-        rightImage = cv2.imread('uploads/' + file)
+        rightImage = cv2.imread('static/' + file)
         status, stitched_image = stitcher.stitch((leftImage, rightImage))  
   
         if status == cv2.Stitcher_OK:
             print("Stitching successful!")
             leftImage = stitched_image
-            cv2.imwrite("uploads/result.jpg", stitched_image)
+            cv2.imwrite("static/result.jpg", stitched_image)
             count+=1
         else:
             print("Stitching failed!")
@@ -42,13 +48,13 @@ def stritchImages():
 
 
 def cannyEdgeDetection():
-    orginalImage = cv2.imread("uploads/result.jpg")
+    orginalImage = cv2.imread("static/result.jpg")
     grayImage = cv2.cvtColor(orginalImage, cv2.COLOR_BGR2GRAY)
     median_value = np.median(grayImage)
     lower_threshold = int(max(0, 0.7 * median_value))
     upper_threshold = int(min(255, 1.3 * median_value))
     canny_edges = cv2.Canny(grayImage, lower_threshold, upper_threshold)
-    cv2.imwrite("uploads/cannyResult.jpg", canny_edges)
+    cv2.imwrite("static/cannyResult.jpg", canny_edges)
 
 @app.route('/', methods=['GET', 'POST'])
 def uploadImages():
